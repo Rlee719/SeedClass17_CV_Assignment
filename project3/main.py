b0962248ec87d030eda90b5e6dc1fc87a3ab0f64
郭一兴 <843840655@qq.com>
2020-03-24 11:21:39 +0800

    去掉bn

--- project3/main.py
+++ project3/main.py
@@ -27,30 +27,30 @@ if __name__ == "__main__":
 
 
     models = [
-                [[3072,190,10],[bpnn.relu,bpnn.relu,bpnn.relu,bpnn.relu]],
+                [[3072,100,100,50,10],[bpnn.relu,bpnn.relu,bpnn.relu,bpnn.relu,bpnn.relu]],
             ]
-    lrs = [1.0]
+    lrs = [1e-3]
     for m in models:
         for lr in lrs:
-            classifier = bpnn.BPNN(m, 'he')
+            classifier = bpnn.BPNN(m, 'xavier')
             optimizer = bpnn.Optimizer(classifier)
             
             optimizer.lr = lr
             optimizer.lr_decay = bpnn.Learning_rate_decay.exp
-            optimizer.lr_k = 0.1
-            optimizer.reg_type = bpnn.Regularization.L2
-            optimizer.momentum_type = bpnn.Momentum.Momentum
+            optimizer.lr_k = 0
+            optimizer.momentum_type = bpnn.Momentum.Adagrad
             optimizer.mu = 0.8
-            optimizer.reg = 0
+            optimizer.reg_type = bpnn.Regularization.none
+            optimizer.reg = 0.075
 
             classifier.useOpt(optimizer)
 
             print("Doing: train net")
-            train_loss_list, train_acc_list = classifier.train(X_train, y_train,epoch=30, batch_size=200)
+            train_loss_list, train_acc_list = classifier.train(X_train, y_train,epoch=50, batch_size=200)
             print(train_loss_list[-1], train_acc_list[-1])
 
-    # print(train_loss_list)
-    # print("Doing: test net")
-    # test_acc = classifier.evaluate(X_test, y_test)
+    print(train_loss_list)
+    print("Doing: test net")
+    test_acc = classifier.evaluate(X_test, y_test)
 
-    # print("test accuracy is %f" % (test_acc))
+    print("test accuracy is %f" % (test_acc))
