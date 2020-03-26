77f2115aef72d8d94125fd247050beaf65744783
rlee719 <rlee719@outlook.com>
2020-03-26 16:14:00 +0800

    project4, megengine resnet demo

--- project3/main.py
+++ project3/main.py
@@ -2,16 +2,8 @@ import numpy as np
 # import minpy.numpy as np
 import bpnn
 import data_utils
-
-if __name__ == "__main__":
-    '''
-    print("Doing: load image data")
-    X_train, y_train, X_test, y_test = data_utils.load_CIFAR10('../cifar-10-batches-py')
-
-    # 图像左右翻转增加数据集
-    print("Doing: image enhance")
-    data_utils.create_image_enhance_npy(X_train, X_test, y_train, y_test, "../")
-    '''
+from plot import *
+def act_type():
     
     # 加载 npy 文件
     print("Doing: load npy files")
@@ -27,10 +19,14 @@ if __name__ == "__main__":
 
 
     models = [
-                [[3072,100,100,50,10],[bpnn.relu,bpnn.relu,bpnn.relu,bpnn.relu,bpnn.relu]],
+                [[3072,200,100,50,10],[bpnn.relu,bpnn.relu,bpnn.relu,bpnn.relu,bpnn.relu]],
+                [[3072,200,100,50,10],[bpnn.sigmoid, bpnn.sigmoid, bpnn.sigmoid, bpnn.sigmoid, bpnn.sigmoid]],
+                [[3072,200,100,50,10],[bpnn.tanh,bpnn.tanh,bpnn.tanh,bpnn.tanh,bpnn.tanh]],
+                [[3072,200,100,50,10],[bpnn.unact,bpnn.unact,bpnn.unact,bpnn.unact,bpnn.unact]]
             ]
-    lrs = [1e-3]
-    for m in models:
+    lrs = [1e-2]
+    act = ["relu", "sigmoid", "tanh", "unact"]
+    for i, m in enumerate(models):
         for lr in lrs:
             classifier = bpnn.BPNN(m, 'xavier')
             optimizer = bpnn.Optimizer(classifier)
@@ -40,17 +36,30 @@ if __name__ == "__main__":
             optimizer.lr_k = 0
             optimizer.momentum_type = bpnn.Momentum.Adagrad
             optimizer.mu = 0.8
-            optimizer.reg_type = bpnn.Regularization.none
-            optimizer.reg = 0.075
+            optimizer.reg_type = bpnn.Regularization.L2
+            optimizer.reg = 0.00075
 
             classifier.useOpt(optimizer)
 
             print("Doing: train net")
-            train_loss_list, train_acc_list = classifier.train(X_train, y_train,epoch=50, batch_size=200)
+            train_loss_list, train_acc_list = classifier.train(X_train, y_train, epoch=50, batch_size=64)
             print(train_loss_list[-1], train_acc_list[-1])
+            draw_plot(range(len(train_loss_list)), train_loss_list, "loss_" + act[i], "epoch", "loss", "loss_" + act[i])
+            draw_plot(range(len(train_acc_list)), train_acc_list, "train_acc_" + act[i], "epoch", "train_acc", "train_acc_" + act[i])
+            test_acc = classifier.evaluate(X_test, y_test)
+            print("test accuracy is %f" % (test_acc))
 
-    print(train_loss_list)
-    print("Doing: test net")
-    test_acc = classifier.evaluate(X_test, y_test)
+    #print(train_loss_list)
+    #print("Doing: test net")
 
-    print("test accuracy is %f" % (test_acc))
+
+if __name__ == "__main__":
+    act_type()
+    '''
+    print("Doing: load image data")
+    X_train, y_train, X_test, y_test = data_utils.load_CIFAR10('../cifar-10-batches-py')
+
+    # 图像左右翻转增加数据集
+    print("Doing: image enhance")
+    data_utils.create_image_enhance_npy(X_train, X_test, y_train, y_test, "../")
+    '''
